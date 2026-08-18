44246724401a5a3e74b47eddcd709fe72c17ff29
Never1Surrender <thays.oliveira.20121@gmail.com>
2026-08-18 11:09:05 -0300

    test: reverte quebra proposital do assert de TC 7

--- tests/testCases.spec.js
+++ tests/testCases.spec.js
@@ -7,7 +7,7 @@ test('TC 7: Verificar a página de Test Cases', async ({ page }) => {
   await page.goto(BASE_URL);
   await page.locator('#header').getByRole('link', { name: /Test Cases/ }).click();
 
-  await expect(page).toHaveURL(/\/forcando-falha-para-testar-integracao-jira/);
+  await expect(page).toHaveURL(/\/test_cases/);
 
   const tituloTestCases = page.getByRole('heading', { name: 'Test Cases', exact: true });
   await expect(tituloTestCases).toBeVisible();
